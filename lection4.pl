5cd75bb188fd6473d27b6dc23ef015a3056bf043
Unknown <arseniyzhuck@mail.ru>
2020-03-24 20:55:16 +0300

    Sub_set
    Beatiful_subset

--- lection4.pl
+++ lection4.pl
@@ -22,8 +22,6 @@ write_str([H|T]):-put(H),write_str(T).
 build_all_razm_p:-
 		read_str(A,N),read(K),b_a_rp(A,K,[]).
 		
-in_list([El|_],El).
-in_list([_|T],El):-in_list(T,El).
 b_a_rp(A,0,Perm1):-write_str(Perm1),nl,!,fail.
 b_a_rp(A,N,Perm):-in_list(A,El),N1 is N-1,b_a_rp(A,N1,[El|Perm]).
 
@@ -44,47 +42,8 @@ sub_set([],[]).
 sub_set([H|Sub_set],[H|Set]):-sub_set(Sub_set,Set).
 sub_set(Sub_set,[H|Set]):-sub_set(Sub_set,Set).
 
+r_list(A,N):-r_list(A,N,0,[]).
+r_list(A,N,N,A):-!.
+r_list(A,N,K,B):-read(X),append(B,[X],B1),K1 is K+1,r_list(A,N,K1,B1).
 
-sprava_next(A,B,[C]):-fail.
-sprava_next(A,B,[A|[B|Tail]]).
-sprava_next(A,B,[_|List]):-sprava_next(A,B,List).
-
-sleva_next(A,B,[C]):-fail.
-sleva_next(A,B,[B|[A|Tail]]).
-sleva_next(A,B,[_|List]):-sleva_next(A,B,List).
-
-next_to(A,B,List):-sprava_next(A,B,List).
-next_to(A,B,List):-sleva_next(A,B,List).
-
-el_no(List,Num,El):-el_no(List,Num,1,El).
-el_no([H|_],Num,Num,H):-!.
-el_no([_|Tail],Num,Ind,El):-Ind1 is Ind+1,el_no(Tail,Num,Ind1,El).
-
-pr_ein:-Houses[_,_,_,_,_],
-		
-		in_list(Houses,[red,english,_,_,_]),
-		in_list(Houses,[_,spanish,_,dog,_]),
-		in_list(Houses,[green,_,coffee,_,_]),
-		in_list(Houses,[_,ukrain,tea,_,_]),
-		sprava_next([green,_,_,_,_],[white,_,_,_,_],Houses),
-		in_list(Houses,[_,_,_,ulitka,old_gold]),
-		in_list(Houses,[yellow,_,_,_,kool]),
-		el_no(Houses,3,[_,_,milk,_,_]),
-		el_no(Houses,1,[_,norway,_,_,_]),
-		next_to([_,_,_,_,chester],[_,_,_,fox,_],Houses),
-		next_to([_,_,_,_,kool],[_,_,_,horse,_],Houses),
-		in_list(Houses,[_,_,orange,_,lucky]),
-		in_list(Houses,[_,japan,_,_,parlament]),
-		next_to([_,norway,_,_,_],[blue,_,_,_,_],Houses),
-		
-		
-		in_list(Houses,[_,WHO1,water,_,_]),
-		in_list(Houses,[_,WHO2,_,zebra,_]),
-		write(Houses),
-		write(WHO1),nl,write(WHO2).
-		
-		
-
-
-
-
+pr_subset:-read(N),r_list(A,N),sub_set(B,A),write(B),nl,fail.
