ef69c0b2a831356c2a894777402c4cfcd3d3d51e
Unknown <arseniyzhuck@mail.ru>
2020-04-03 15:57:49 +0300

    DOP_IZ

--- logical.pl
+++ logical.pl
@@ -95,3 +95,27 @@ pr_log:-Seek=[_,_,_,_],
     write_place_people(banker,Seek),
     write_place_people(bloger,Seek),
     write_place_people(official,Seek).
+
+
+pr_ein1:-Event=[_,_,_,_],
+    in_list(Event,[funeral,italy,_,_]),
+    in_list(Event,[wedding,france,_,_]),
+    in_list(Event,[_,france,_,15]),
+    in_list(Event,[rest,china,_,_]),
+    in_list(Event,[conference,_,programmer,_]),
+    in_list(Event,[_,usa,_,1]),
+    in_list(Event,[_,_,official,45]),
+    in_list(Event,[_,_,banker,6]),
+    in_list(Event,[_,_,blogger,_]),
+    not(in_list(Event,[_,china,blogger,_])),
+    not(in_list(Event,[_,china,official,_])),
+
+    in_list(Event,[funeral,F1,F2,F3]),
+    in_list(Event,[wedding,S1,S2,S3]),
+    in_list(Event,[rest,T1,T2,T3]),
+    in_list(Event,[conference,Frth1,Frth2,Frth3]),
+
+    write("funeral "),write(F1),write(" "),write(F2),write(" "),write(F3),nl,
+    write("wedding "),write(S1),write(" "),write(S2),write(" "),write(S3),nl,
+    write("rest "),write(T1),write(" "),write(T2),write(" "),write(T3),nl,
+    write("conference "),write(Frth1),write(" "),write(Frth2),write(" "),write(Frth3).
